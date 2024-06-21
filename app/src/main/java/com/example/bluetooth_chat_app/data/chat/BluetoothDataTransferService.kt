60a50868d7112a23497301a7d0acb47e1a35cadb
Prince <banerjee.prince69@gmail.com>
2024-06-21 14:03:43 +0530

    WIP

---
+++ app/src/main/java/com/example/bluetooth_chat_app/data/chat/BluetoothDataTransferService.kt
@@ -0,0 +1,9 @@
+package com.example.bluetooth_chat_app.data.chat
+
+import android.bluetooth.BluetoothSocket
+
+class BluetoothDataTransferService(
+    private val socket: BluetoothSocket
+) {
+
+}
