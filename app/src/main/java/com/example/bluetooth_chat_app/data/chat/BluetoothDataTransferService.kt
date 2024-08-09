77c20cea0c53822292ecbded182f8f37b169f0c1
Prince <banerjee.prince69@gmail.com>
2024-08-09 16:09:39 +0530

    Final(2)

--- app/src/main/java/com/example/bluetooth_chat_app/data/chat/BluetoothDataTransferService.kt
+++ app/src/main/java/com/example/bluetooth_chat_app/data/chat/BluetoothDataTransferService.kt
@@ -1,9 +1,16 @@
 package com.example.bluetooth_chat_app.data.chat
 
 import android.bluetooth.BluetoothSocket
+import com.example.bluetooth_chat_app.domain.chat.ConnectionResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class BluetoothDataTransferService(
     private val socket: BluetoothSocket
 ) {
+    fun listenIncomingMessages(): Flow<ConnectionResult>{
+        return flow {
 
+        }
+    }
 }
